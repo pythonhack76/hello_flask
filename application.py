b64e3dce858518421f1d52a70ac96d47a88ffd48
pythonthehack76 <lrulvoni@gmail.com>
2021-02-27 21:40:42 +0100

    update

--- application.py
+++ application.py
@@ -1,11 +1,16 @@
 from flask import Flask
-
+#primo semplice programma utilizzando FLASK in modalità virtuasle con ENV
 app = Flask(__name__)
-
+#creazione pagina home
 @app.route("/")
 def homepage():
     return "<h1 style='color:red'>Nuovo titolo</h1>"
 
+#utilizzo decoratori per collegamento pagine
 @app.route("/contatti")
 def contatti():
-    return "Contattaci!"
+    return "<span style='color:grey'>Contattaci!</span>"
+
+@app.route("/chi-siamo")
+def chi_siamo():
+    return "Chi siamo?"
